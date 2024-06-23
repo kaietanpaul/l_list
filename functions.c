1cc5a5e6ca26a8d9fdf2d43a7b10dc7e63225729
Kaietan Paul <kaietanpaul@gmail.com>
2024-06-23 18:59:56 +0200

    strdup, strlen, malloc, free, no leaks, add_movie update

--- functions.c
+++ functions.c
@@ -1,18 +1,6 @@
 #include "l_list.h"
 
-t_movies	*add_movie(t_movies *head, char *title, char *director, int year)
-{
-	t_movies *movie = malloc(sizeof(t_movies));
-	if (!movie)
-		return (NULL);
-	movie->title = title;
-	movie->director = director;
-	movie->year = year;
-	movie->next = head;
-	return (movie);
-}
-
-void	print_movie(t_movies *head)
+void		print_movie(t_movies *head)
 {
 	if (head)
 	{
@@ -22,7 +10,7 @@ void	print_movie(t_movies *head)
 	}
 }
 
-void	print_all_movies(t_movies *head)
+void		print_all_movies(t_movies *head)
 {
 	while (head != NULL)
 	{
@@ -37,6 +25,49 @@ void		free_all_movies(t_movies *head)
 	{
 		t_movies *temp = head;
 		head = head->next;
+		free(temp->director);
+		free(temp->title);
 		free(temp);
 	}
+}
+
+int 		kai_strlen(const char *str)
+{
+	int	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+char 		*kai_strdup(const char *str)
+{
+	int i = 0;
+	char *copied_str = malloc(kai_strlen(str) + 1);
+	if (!copied_str)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		copied_str[i] = str[i];
+		i++;
+	}
+	copied_str[i] = '\0';
+
+	return (copied_str);
+}
+
+t_movies	*add_movie(t_movies *head, char *title, char *director, int year)
+{
+	t_movies *movie = malloc(sizeof(t_movies));
+	if (!movie)
+		return (NULL);
+
+	movie->title = kai_strdup(title);
+	movie->director = kai_strdup(director);
+	movie->year = year;
+	movie->next = head;
+
+	return (movie);
 }
--- l_list.h
+++ l_list.h
@@ -12,9 +12,14 @@ typedef struct s_movies
 	struct s_movies	*next;
 }	t_movies;
 
-t_movies	*add_movie(t_movies *head, char *title, char *director, int year);
-void		print_movie(t_movies *head);
-void		print_all_movies(t_movies *head);
-void		free_all_movies(t_movies *head);
+t_movies	*add_movie(t_movies *, char *, char *, int);
+
+void		print_movie(t_movies *);
+void		print_all_movies(t_movies *);
+void		free_all_movies(t_movies *);
+
+int			kai_strlen(const char *);
+
+char 		*kai_strdup(const char *str);
 
 #endif
